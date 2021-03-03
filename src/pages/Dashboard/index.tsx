17105bfa505bfb4deb21ef13973fb43c391af15d
Hugo Souza <hugo.souza@ifood.com.br>
2021-03-03 09:33:57 -0300

    Add schedule appointment component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { Container, Header, HeaderContent, Profile, Content, Schedule, NextAppointment, Calendar } from './styles';
 
 import logoImg from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () =>  {
@@ -27,6 +27,30 @@ const Dashboard: React.FC = () =>  {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários Agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src="https://avatars.githubusercontent.com/u/384398?s=460&u=07daf30d96adf7f0c763345a806d10d5b612cf15&v=4" alt="foto" />
+              <strong>Hugo Souza</strong>
+              <span>
+                <FiClock />
+                09:16
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
+
     </Container>
   );
 }
