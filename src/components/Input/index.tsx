ab6ba42dae4d09b6790978618dafbb95460a9b7d
Hugo Souza <hugo.souza@ifood.com.br>
2021-05-05 11:01:30 -0300

    Add component specs

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -39,7 +39,13 @@ const Input: React.FC<InputProps> = ({
   }, [fieldName, registerField]);
 
   return (
-    <Container style={containerStyle} isErrored={!!error} isFocused={isFocused} isFilled={isFilled}>
+    <Container
+      style={containerStyle}
+      isErrored={!!error}
+      isFocused={isFocused}
+      isFilled={isFilled}
+      data-testid="input-container"
+    >
       { Icon && <Icon size={20} /> }
       <input
         onFocus={handleFocus}
